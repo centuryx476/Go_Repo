eea3b5d34f639495fe815cc8ce784d9d79ef5057
SNoorizadeh <SNoorizadeh@SandWP.com>
2021-10-09 22:40:38 -0400

    Adding Comments

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
-package main
+package main // This can be named whatever you want
 
-import "fmt"
+import "fmt" // Import "library" for output
 
+// Func has to be named 'main'
 func main() {
 	fmt.Println("Hello from Go!")
 }
+
+// End of Line
