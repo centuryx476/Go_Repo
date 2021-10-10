f2319774762b04661a3c013ac41c49a174646a2d
SNoorizadeh <SNoorizadeh@SandWP.com>
2021-10-10 16:34:54 -0400

    Playing with functions

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt" // Import "library" for output
 
 // Func has to be named 'main'
 func main() {
-	fmt.Println("Hello from Go!")       // Prints on NewLine
-	fmt.Print("This is some text")      // Does not print on NewLine
-	fmt.Print("This is some more text") // Does not print on NewLine
+	fmt.Println("Hello from Go!") // Prints on NewLine
+
+	// Call the below function
+	sayHelloWorld("Passing string to function for output")
+}
+
+// Function for Output
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
 
 // End of Line
