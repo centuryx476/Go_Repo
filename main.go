b9cca26a6de0b7c50bbf0064c7ec11d8328dd68e
centuryx476 <snoorizadeh@hotmail.com>
2021-10-11 21:03:16 -0400

    Learning how to use variables with shorthand :=

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ func main() {
 	fmt.Println("Hello from Go!") // Prints on NewLine
 
 	// One way of creating a variable
-	var whatToSay string
-	whatToSay = "Passing string to function for output"
+	//var whatToSay string
+	//whatToSay = "Passing string to function for output"
+
+	// Another way of creating a variable (Shorthand way)
+	whatToSay2 := "Passing string to function for output2"
 
 	// Call the below function
-	sayHelloWorld(whatToSay)
+	sayHelloWorld(whatToSay2)
 }
 
 // Function for Output
