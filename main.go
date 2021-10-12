90233c54880a5267cf41deb32ed14244eb8b4345
centuryx476 <snoorizadeh@hotmail.com>
2021-10-11 21:01:20 -0400

    Learning how to use variables

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import "fmt" // Import "library" for output
 func main() {
 	fmt.Println("Hello from Go!") // Prints on NewLine
 
+	// One way of creating a variable
+	var whatToSay string
+	whatToSay = "Passing string to function for output"
+
 	// Call the below function
-	sayHelloWorld("Passing string to function for output")
+	sayHelloWorld(whatToSay)
 }
 
 // Function for Output
 func sayHelloWorld(whatToSay string) {
 	fmt.Println(whatToSay)
 }
-
-// End of Line
