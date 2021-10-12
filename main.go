a15dcb6ad91e5771898a59b4ddab07985894f8b0
SNoorizadeh <SNoorizadeh@SandWP.com>
2021-10-11 21:23:13 -0400

    Fixing Project Structure

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main // This can be named whatever you want
 
-import "fmt" // Import "library" for output
+import (
+	"fmt"
+	"myapp/doctor"
+)
 
 // Func has to be named 'main'
 func main() {
-	fmt.Println("Hello from Go!") // Prints on NewLine
+	var whatToSay string
 
-	// One way of creating a variable
-	//var whatToSay string
-	//whatToSay = "Passing string to function for output"
-
-	// Another way of creating a variable (Shorthand way)
-	whatToSay2 := "Passing string to function for output2"
-
-	// Call the below function
-	sayHelloWorld(whatToSay2)
-}
-
-// Function for Output
-func sayHelloWorld(whatToSay string) {
+	whatToSay = doctor.Intro()
 	fmt.Println(whatToSay)
+
 }
